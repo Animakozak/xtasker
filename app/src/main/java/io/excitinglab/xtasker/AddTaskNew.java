47fdea19552b551b002b85a301a7cf4459af3375
Animakozak <animakozak@ex.ua>
2017-05-12 02:24:49 +0300

    Another update to AddTaskNew activity

--- app/src/main/java/io/excitinglab/xtasker/AddTaskNew.java
+++ app/src/main/java/io/excitinglab/xtasker/AddTaskNew.java
@@ -2,23 +2,26 @@ package io.excitinglab.xtasker;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-public class AddTaskNew extends AppCompatActivity {
-    ImageButton btnDue, btnRem;
+public class AddTaskNew extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
+    TextView btnDue, btnRem;
     String dateString, timeString;
+    int btnID;
     int hour_x, minute_x, day_x, month_x, year_x;
-    static final int DUE_DIALOG_ID = 0;
-    static final int REM_DIALOG_ID = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,55 +33,62 @@ public class AddTaskNew extends AppCompatActivity {
         year_x = today.get(Calendar.YEAR);
         hour_x = today.get(Calendar.HOUR_OF_DAY);
         minute_x = today.get(Calendar.MINUTE);
-        showDialogOnButtonClick();
-    }
+        btnDue = (TextView) findViewById(R.id.editDueDate);
+        btnRem = (TextView) findViewById(R.id.editReminder);
 
-    public void showDialogOnButtonClick(){
-        btnDue = (ImageButton)findViewById(R.id.buttonDueDate);
-        btnRem = (ImageButton)findViewById(R.id.buttonReminder);
+
         btnDue.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                showDialog(DUE_DIALOG_ID);
-                TextView dueDate = (TextView) findViewById(R.id.editDueDate);
-                dueDate.setText(dateString);
+            public void onClick(View v1) {
+                btnID=1;
+                DatePickerDialog dpickerListner = new DatePickerDialog(AddTaskNew.this, AddTaskNew.this,
+                        year_x, month_x, day_x);
+                dpickerListner.show();
+
             }
         });
+
         btnRem.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                showDialog(DUE_DIALOG_ID);
-                showDialog(REM_DIALOG_ID);
-                TextView reminderDate = (TextView) findViewById(R.id.editReminder);
-                reminderDate.setText(timeString+" "+dateString);
+            public void onClick(View v2) {
+                btnID=2;
+                DatePickerDialog dtpickerListner = new DatePickerDialog(AddTaskNew.this, AddTaskNew.this,
+                        year_x, month_x, day_x);
+                dtpickerListner.show();
             }
         });
     }
 
     @Override
-    protected Dialog onCreateDialog(int id){
-        if(id == DUE_DIALOG_ID) return new DatePickerDialog(this, dpickerListner, year_x, month_x, day_x);
-        else if(id == REM_DIALOG_ID) return new TimePickerDialog(this, tpickerListner, hour_x, minute_x, true);
-        else return null;
-    }
-
-    private DatePickerDialog.OnDateSetListener dpickerListner = new DatePickerDialog.OnDateSetListener(){
-
-        @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        if(btnID == 1){
             year_x = year;
-            month_x = monthOfYear;
+            month_x = month;
             day_x = dayOfMonth;
             dateString = day_x+"."+month_x+"."+year_x;
+            btnDue.setText(dateString);
         }
-    };
+        if(btnID == 2){
+            year_x = year;
+            month_x = month;
+            day_x = dayOfMonth;
+            dateString = day_x+"."+month_x+"."+year_x;
 
-    private TimePickerDialog.OnTimeSetListener tpickerListner = new TimePickerDialog.OnTimeSetListener() {
-        @Override
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            hour_x = hourOfDay;
-            minute_x = minute;
-            timeString = hour_x+":"+minute_x;
+            TimePickerDialog tpickerListner = new TimePickerDialog(AddTaskNew.this, AddTaskNew.this, hour_x, month_x, true);
+            tpickerListner.show();
         }
-    };
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        hour_x = hourOfDay;
+        minute_x = minute;
+        timeString = hour_x+":"+minute_x;
+        TextView reminderDate = (TextView) findViewById(R.id.editReminder);
+        btnRem.setText(timeString+" "+dateString);
+    }
 }
+
+
+
+
